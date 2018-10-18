add53d2dd027a56a8fd1a482e92b0e0496ac039c
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-18 14:32:03 -0300

    adicionando product-list

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,18 +3,28 @@ import api from "../../services/api";
 
 export default class Main extends Component {
 
+    state = {
+        products: []
+    };
+
     componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get('/products');
-        console.log(response);
+        this.setState({ products: response.data.docs });
     }
 
     render() {
         return (
-            <h1>Hello World</h1>
+            <div className="product-list">
+                {
+                    this.state.products.map(product => (
+                        <h2 key={product._id}>{product.title}</h2>
+                    ))
+                }
+            </div>
         );
     }
 }
